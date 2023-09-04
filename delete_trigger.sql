31236347aed02fdad6ca63696b0555fca085f8c3
Maksym Miskevych <120181270+maks787@users.noreply.github.com>
2023-09-04 10:32:04 +0300

    Update delete_trigger.sql

--- delete_trigger.sql
+++ delete_trigger.sql
@@ -1,1 +1,9 @@
-
+CREATE TRIGGER guestKustutamine
+ON guest
+FOR DELETE
+AS
+INSERT INTO logi(kuupaev, kasutaja, andmed, tegevus)
+SELECT GETDATE(), USER, 
+CONCAT(deleted.first_name, ', ', deleted.last_name),
+'guest on kustatud'
+FROM deleted
