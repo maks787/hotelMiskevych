5f48b73befa2e651f5745f2ab27264cb1608702d
Maksym Miskevych <120181270+maks787@users.noreply.github.com>
2023-09-27 13:07:51 +0300

    Update functions.sql

--- functions.sql
+++ functions.sql
@@ -3,6 +3,8 @@ CREATE FUNCTION fn_ILTVF_GetEmployees()
   as
   return(select id,name,cast(dateofbirth as date) AS BOD
   from tblemployees)
+  --создает и возвращает результат в виде таблицы.
+  
 CREATE FUNCTION fn_MSTVF_GetEmployees()
 returns @table table (id int,Name nvarchar(20),DOB Date)
 as
@@ -12,6 +14,7 @@ Select id,name,cast(DateOfBirth as Date)
 From tblEmployees
 return
 end
+--функция возвращает таблицу с данными о сотрудниках из таблицы tblEmployees, где столбец DateOfBirth преобразуется в формат типа данных Date. 
 
 CREATE FUNCTION fn_getEmployeesNameByid(@id int)
 returns nvarchar(20)
@@ -26,7 +29,7 @@ as
 begin
 return(select name from tblEmployees where id=@id)
 end
-
+--таблицы возвращают имя сотрудника из таблицы tblEmployees по заданному идентификатору (id).
 alter function fn_getEmployeesNameById(@id int)
 returns nvarchar(20)
 with schemaBinding
@@ -34,3 +37,5 @@ as
 begin
 return(select name from dbo.tblEmployees where id=@id)
 end
+--функция привязана к схеме базы данных,функция принимает один аргумент @id типа int и возвращает имя сотрудника с этим идентификатором из таблицы dbo.tblEmployees. 
+
