4c8743bc69b1f265f0bf74f16e66ae671cf57297
Maksym Miskevych <120181270+maks787@users.noreply.github.com>
2023-09-27 12:27:49 +0300

    Update view.sql

--- view.sql
+++ view.sql
@@ -1,1 +1,100 @@
+Create view vWEmployeeByDepartment
+as
+Select id,name,salary,gender,deptname
+from tblEmployee
+join tblDepartment
+on tblEmployee.DepartmentId = tblDepartment.DeptId
+ 
+ select * from vWEmployeeByDepartment
+ 
+
+Create view vWITDepartment_Employees
+as
+Select id,name,salary,gender,deptname
+from tblEmployee
+join tblDepartment
+on tblEmployee.DepartmentId = tblDepartment.DeptId
+where tblDepartment.DeptName='IT'
+
+select * from vWITDepartment_Employees
+ 
+Create view vWEmployeesNonConfidentialData
+as
+Select id,name,gender,deptname
+from tblEmployee
+join tblDepartment
+on tblEmployee.DepartmentId = tblDepartment.DeptId
+select * from vWEmployeesNonConfidentialData
+ 
+create view vWemployeesCountByDepartment
+as
+Select DeptName,COUNT(ID) as totalEmployees
+from tblEmployee
+join tblDepartment
+on tblEmployee.DepartmentId = tblDepartment.DeptId
+Group by DeptName
+select * from vWemployeesCountByDepartment
+ 
+Create view vWEmployeeByDepartment
+as
+Select id,name,salary,gender,deptname
+from tblEmployee
+join tblDepartment
+on tblEmployee.DepartmentId = tblDepartment.DeptId
+ 
+ select * from vWEmployeeByDepartment
+ 
+
+Create view vWITDepartment_Employees
+as
+Select id,name,salary,gender,deptname
+from tblEmployee
+join tblDepartment
+on tblEmployee.DepartmentId = tblDepartment.DeptId
+where tblDepartment.DeptName='IT'
+
+select * from vWITDepartment_Employees
+ 
+Create view vWEmployeesNonConfidentialData
+as
+Select id,name,gender,deptname
+from tblEmployee
+join tblDepartment
+on tblEmployee.DepartmentId = tblDepartment.DeptId
+select * from vWEmployeesNonConfidentialData
+ 
+create view vWemployeesCountByDepartment
+as
+Select DeptName,COUNT(ID) as totalEmployees
+from tblEmployee
+join tblDepartment
+on tblEmployee.DepartmentId = tblDepartment.DeptId
+Group by DeptName
+select * from vWemployeesCountByDepartment
+ 
+create view vWtotalSalesByProduct
+with schemaBinding
+as
+Select name,
+Sum(ISNULL((QuantitySold * UnitPrice),0)) as totalSales,
+Count_Big(*) as totalTransactions
+from dbo.tblProductSales
+join dbo.tblProduct
+on dbo.tblProduct.ProductId = dbo.tblProductSales.ProdutctId
+group by name
+
+select * from vWtotalSalesByProduct
+ 
+
+create unique clustered index UIX_vWTotalSalesByProduct_Name
+on vWTotalSalesByProduct(Name)
+Create View vWEmployeeDetailsSorted
+as
+Select Id, Name, Gender, DepartmentId
+from tblEmployee
+order by Id
+Create View vwOnTempTable
+as
+Select Id, Name, Gender
+from ##TestTempTable
 
